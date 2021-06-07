180564e2705a94f666e3c73dbc736bdbf4e53662
Ekwiwat <74827964+Ekwiwat@users.noreply.github.com>
2021-06-07 12:43:33 +0700

    Initial commit

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+}
